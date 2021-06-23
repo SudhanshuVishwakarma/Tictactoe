4c4b35c65f90d65d7f3ae51650ff372f6fb6ee9f
itsok <vsudhanshu18@gmail.com>
2021-06-23 18:56:58 +0530

    sq

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React from 'react';
+import Board from './Components/Board';
 
 const App = () => {
   return (
     <div>
-      <h1>Welcome to React Parcel Micro App!</h1>
-      <p>Hard to get more minimal than this React app.</p>
+      <h1>TIC TAC TOE</h1>
+      <Board />
     </div>
   );
 };
