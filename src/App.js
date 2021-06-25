a55ce1a530eb6d0558398210ebce075db2c06e44
itsok <vsudhanshu18@gmail.com>
2021-06-25 12:46:53 +0530

    day 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Style/root.scss';
 import Board from './Components/Board';
+import { calculateWinner } from './helper';
 
 const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const winner = calculateWinner(board);
+
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next Player is ${isXNext ? 'X' : 'O'}`;
+
+  const handleSquareClick = position => {
+    if (board[position] || winner) {
+      return;
+    }
+
+    setBoard(prev => {
+      return prev.map((Square, pos) => {
+        if (pos === position) {
+          return isXNext ? 'X' : 'O';
+        }
+        return Square;
+      });
+    });
+    setIsXNext(prev => !prev);
+  };
   return (
-    <div>
+    <div className="app">
       <h1>TIC TAC TOE</h1>
-      <Board />
+      <h2>{message}</h2>
+      <Board board={board} handleSquareClick={handleSquareClick} />
     </div>
   );
 };
--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,7 +1,12 @@
 import React from 'react';
-
-const Square = ({ value }) => {
-  return <button type="button">{value}</button>;
+const Square = ({ value, onClick }) => {
+  return (
+    <button type="button" className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
 };
 
 export default Square;
+
+
